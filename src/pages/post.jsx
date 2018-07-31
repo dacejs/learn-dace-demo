2b3af5e03f52315cec58d78fd8121397be117a03
zhongzhi107 <zhong.zhi@163.com>
2018-07-31 10:48:34 +0800

    chore: v1.0.0-beta.8

--- src/pages/post.jsx
+++ src/pages/post.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Layout from '../../layouts/default';
+import Layout from '../layouts/default';
 
 const Post = (props) => {
   const { query } = props.location;
